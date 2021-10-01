bca8d0d4fd2c749d7844f70f4f477509a673e3ec
Luisa0218 <78153272+Luisa0218@users.noreply.github.com>
2021-10-01 11:34:38 -0500

    Basic project creation

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: 'Cupertino App',
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Cupertino App Bar'),
+        ),
+        child: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
