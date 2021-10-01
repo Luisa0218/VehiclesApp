bcc617c4ee04026881dfe80bd2cf3cf53736a4df
Luisa0218 <78153272+Luisa0218@users.noreply.github.com>
2021-10-01 12:07:52 -0500

    Begin login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vehicles_app/screens/login_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      title: 'Cupertino App',
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('Cupertino App Bar'),
-        ),
-        child: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vehicles App',
+      home: const LoginScreen(),
     );
   }
 }
