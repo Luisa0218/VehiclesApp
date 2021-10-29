5ca48231ea677f62e2fff752c964d3d933ff3005
Luisa0218 <78153272+Luisa0218@users.noreply.github.com>
2021-10-29 17:12:39 -0500

    Changes_user_screen2

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:vehicles_app/components/loader_component.dart';
 import 'package:vehicles_app/helpers/api_helper.dart';
+import 'package:vehicles_app/models/document%20_type.dart';
 import 'package:vehicles_app/models/response.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/models/user.dart';
@@ -27,11 +28,56 @@ class _UserScreenState extends State<UserScreen> {
   // ignore: prefer_final_fields
   TextEditingController _firstNameController = TextEditingController();
 
+  String _lastName = '';
+  final String _lastNameError = '';
+  final bool _lastNameShowError = false;
+  final TextEditingController _lastNameController = TextEditingController();
+
+  DocumentType _documentType = DocumentType(id: 0, description: '');
+  final List<DocumentType> _documentTypes = [];
+
+  String _document = '';
+  final String _documentError = '';
+  final bool _documentShowError = false;
+  final TextEditingController _documentController = TextEditingController();
+
+  String _address = '';
+  final String _addressError = '';
+  final bool _addressShowError = false;
+  final TextEditingController _addressController = TextEditingController();
+
+  String _email = '';
+  final String _emailError = '';
+  final bool _emailShowError = false;
+  final TextEditingController _emailController = TextEditingController();
+
+  String _phoneNumber = '';
+  final String _phoneNumberError = '';
+  final bool _phoneNumberShowError = false;
+  final TextEditingController _phoneNumberController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     _firstName = widget.user.firstName;
     _firstNameController.text = _firstName;
+
+    _lastName = widget.user.lastName;
+    _lastNameController.text = _lastName;
+
+    _documentType = widget.user.documentType;
+
+    _document = widget.user.document;
+    _documentController.text = _document;
+
+    _address = widget.user.address;
+    _addressController.text = _address;
+
+    _email = widget.user.email;
+    _emailController.text = _email;
+
+    _phoneNumber = widget.user.phoneNumber;
+    _phoneNumberController.text = _phoneNumber;
   }
 
   @override
@@ -46,7 +92,14 @@ class _UserScreenState extends State<UserScreen> {
           SingleChildScrollView(
             child: Column(
               children: <Widget>[
+                _showPhoto(),
                 _showFirstName(),
+                _showLastName(),
+                _showDocumentType(),
+                _showDocument(),
+                _showEmail(),
+                _showAddress(),
+                _showPhoneNumber(),
                 _showButtons(),
               ],
             ),
@@ -247,5 +300,129 @@ class _UserScreenState extends State<UserScreen> {
     }
 
     Navigator.pop(context, 'yes');
+  }
+
+  Widget _showPhoto() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: widget.user.id.isEmpty
+          ? Image(
+              image: AssetImage('assets/noimage.png'),
+              height: 160,
+              width: 160,
+            )
+          : ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/Vehicles_Logos.png'),
+                image: NetworkImage(widget.user.imageFullPath),
+                width: 100,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+            ),
+    );
+  }
+
+  Widget _showLastName() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _lastNameController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa apellidos..',
+          labelText: 'Apellidos',
+          errorText: _lastNameShowError ? _lastNameError : null,
+          suffixIcon: const Icon(Icons.person),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _lastName = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showDocumentType() {
+    return Container();
+  }
+
+  Widget _showDocument() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _documentController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa documento...',
+          labelText: 'Documento',
+          errorText: _documentShowError ? _documentError : null,
+          suffixIcon: const Icon(Icons.assignment_ind),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _document = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa email...',
+          labelText: 'Email',
+          errorText: _emailShowError ? _emailError : null,
+          suffixIcon: const Icon(Icons.email),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _email = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showAddress() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _addressController,
+        keyboardType: TextInputType.streetAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa dirección...',
+          labelText: 'Dirección',
+          errorText: _addressShowError ? _addressError : null,
+          suffixIcon: const Icon(Icons.home),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _address = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showPhoneNumber() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _phoneNumberController,
+        keyboardType: TextInputType.phone,
+        decoration: InputDecoration(
+          hintText: 'Ingresa Teléfono...',
+          labelText: 'Teléfono',
+          errorText: _phoneNumberShowError ? _phoneNumberError : null,
+          suffixIcon: const Icon(Icons.phone),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+        onChanged: (value) {
+          _phoneNumber = value;
+        },
+      ),
+    );
   }
 }
